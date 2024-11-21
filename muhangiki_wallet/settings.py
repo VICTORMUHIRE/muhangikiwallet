cf4bf49f72926d7e6fa4e11f8d8d35dc40095f1c
Adonis-Rwabira <adonisbitigaywa@gmail.com>
2024-11-21 19:04:23 +0200

    first commit

--- muhangiki_wallet/settings.py
+++ muhangiki_wallet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-0n!^rc92pcf=fr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOST', "*")]
+ALLOWED_HOSTS = ["muhangiki-wallet.onrender.com", "*"]
 
 
 # Application definition
